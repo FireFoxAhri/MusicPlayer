665498d2850e82485683d9ac57e43f1cda08aa7b
FireFox <309637962@qq.com>
2017-04-20 22:16:05 +0800

    添加了API加密函数

---
+++ app/src/main/java/com/firefox/musicplayer/ui/main/MainActivity.java
@@ -0,0 +1,11 @@
+package com.firefox.musicplayer.ui.main;
+
+import com.firefox.musicplayer.ui.base.BaseActivity;
+
+/**
+ * Created by FireFox on 2017/4/7.
+ */
+
+public class MainActivity extends BaseActivity {
+
+}
