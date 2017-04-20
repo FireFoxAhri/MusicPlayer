92c80ad8c4eabac31f823fbe8136828465b4bc73
WF_G <1131852344@qq.com>
2017-04-20 22:16:05 +0800

    0.0

---
+++ app/src/main/java/com/firefox/musicplayer/ui/LoginActivity.java
@@ -0,0 +1,11 @@
+package com.firefox.musicplayer.ui;
+
+import com.firefox.musicplayer.ui.base.BaseActivity;
+
+/**
+ * Created by 俗 on 2017/4/20.
+ */
+
+public class LoginActivity extends BaseActivity {
+
+}
---
+++ app/src/main/java/com/firefox/musicplayer/ui/base/BaseActivity.java
@@ -0,0 +1,16 @@
+package com.firefox.musicplayer.ui.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by FireFox on 2017/4/7.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
