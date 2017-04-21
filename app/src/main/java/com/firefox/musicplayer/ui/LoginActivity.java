7867eb9550b1c2665c1bf368b667b08d01e4e47a
FireFox <309637962@qq.com>
2017-04-21 17:31:03 +0800

    添加了API加密函数

--- app/src/main/java/com/firefox/musicplayer/ui/LoginActivity.java
+++ app/src/main/java/com/firefox/musicplayer/ui/LoginActivity.java
@@ -2,21 +2,43 @@ package com.firefox.musicplayer.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBar;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.firefox.musicplayer.R;
 import com.firefox.musicplayer.ui.base.BaseActivity;
+import com.firefox.musicplayer.utils.user.UserLoginService;
+import com.firefox.musicplayer.utils.user.UserUtil;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by 俗 on 2017/4/20.
  */
 
 public class LoginActivity extends BaseActivity {
+    @BindView(R.id.et_account)
+    EditText etAccount;
+    @BindView(R.id.et_password)
+    EditText etPassword;
+    @BindView(R.id.btnLogin)
+    Button btnLogin;
+
+    @OnClick(R.id.btnLogin)
+    public void onViewClicked() {
+        UserUtil.Login(etAccount.getText().toString(), etPassword.getText().toString());
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_layout);
+        ButterKnife.bind(this);
 
-        
+
     }
+
+
 }
--- app/src/main/java/com/firefox/musicplayer/ui/base/BaseActivity.java
+++ app/src/main/java/com/firefox/musicplayer/ui/base/BaseActivity.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.ButterKnife;
+
 /**
  * Created by FireFox on 2017/4/7.
  */
@@ -13,7 +15,8 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        //初始化ButterKnife
+        ButterKnife.bind(this);
         //隐藏用户标题栏
         ActionBar actionBar=getSupportActionBar();
         if(actionBar!=null)
