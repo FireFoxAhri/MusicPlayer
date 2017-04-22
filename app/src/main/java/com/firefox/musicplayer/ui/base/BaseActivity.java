6b52b9e480292970080f6d4656d6f9e00c38661f
FireFox <309637962@qq.com>
2017-04-22 15:06:09 +0800

    添加了API加密函数

--- app/src/main/java/com/firefox/musicplayer/ui/base/BaseActivity.java
+++ app/src/main/java/com/firefox/musicplayer/ui/base/BaseActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 import butterknife.ButterKnife;
 
@@ -15,12 +16,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //隐藏用户标题栏
-        ActionBar actionBar=getSupportActionBar();
-        if(actionBar!=null)
-        {
-            actionBar.hide();
-        }
+
 
     }
 }
--- app/src/main/java/com/firefox/musicplayer/ui/main/MainActivity.java
+++ app/src/main/java/com/firefox/musicplayer/ui/main/MainActivity.java
@@ -3,7 +3,12 @@ package com.firefox.musicplayer.ui.main;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 
+import com.firefox.musicplayer.R;
 import com.firefox.musicplayer.ui.base.BaseActivity;
 import com.firefox.musicplayer.utils.user.UserUtil;
 
@@ -15,12 +20,15 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... params) {
-                UserUtil.Login("13163388532","zjs19941118");
-                return null;
-            }
-        }.execute();
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
     }
 }
